a48870dea06652aa7518612eb659be0d0c59eaa0
RacleRay <racleme@outlook.com>
2023-09-01 09:46:53 +0800

    "lazy sh push"

---
+++ push_git.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "--------- Begin ---------"
+git status
+read -p "Input commit: " msg
+git add .
+git commit -m "$msg"
+git pull
+git push
+echo "Push success: [$msg]"
+echo "---------- End ----------"
+read -p "Press Enter to continue..."
